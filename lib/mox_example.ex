14ee98bcbf1971c2e77ec84e933c3970c001865f
Sean Lewis <sensaisean@gmail.com>
2019-01-10 16:34:08 -0700

    remove puts

--- lib/mox_example.ex
+++ lib/mox_example.ex
@@ -13,7 +13,6 @@ defmodule ExampleAPI do
   alias HTTPoison
   @callback api_post(String.t(), [], []) :: {:ok, nil} | {:error, any()}
   def api_post(body \\ "", headers, options) do
-		IO.puts("WRONG FUNC")
     HTTPoison.post("http://example.com", body, headers, options)
   end
 end
